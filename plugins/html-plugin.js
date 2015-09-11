ef788ee77c5fd3839e03a5ce5aef65d6bcf0e851
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-09-11 09:19:35 -0300

    Basic webpack boilerplate

---
+++ plugins/html-plugin.js
@@ -0,0 +1,80 @@
+/* XXX This code is experimental, it may be subjected to great changes on the
+ * near future.
+ */
+var $ = require('cheerio'),
+    md5 = require('md5'),
+    htmlMinify = require('html-minifier').minify,
+    fs = require('fs'),
+    path = require('path');
+
+function HtmlParserWebpackPlugin() {
+  this.files = Array.prototype.slice.call(arguments);
+  this.production = false;
+}
+
+HtmlParserWebpackPlugin.prototype.apply = function(compiler) {
+  this.compiler = compiler;
+  // XXX I could try to find a better way to check this.
+  // Note: 'compiler.options.debug' options is not ovewritten by cli options
+  // when it is on 'webpack.config.js'.
+  this.production = process.argv.indexOf('--optimize-minimize') !== -1 ||
+                    process.argv.indexOf('-p') !== -1;
+  this.files.forEach(function(file) {
+    compiler.plugin('emit', this.compile.bind(this, file));
+  }, this);
+};
+
+/* Iterate through each element href and src tags and parse them */
+HtmlParserWebpackPlugin.prototype.compile = function(file, compilation, callback) {
+    var input = path.join(this.compiler.context, file);
+    var html = $.load(fs.readFileSync(input));
+    html('[href],[src]').each(function(i, element) {
+      this.parse(element, 'href', compilation);
+      this.parse(element, 'src', compilation);
+    }.bind(this));
+
+    var htmlSource = html.html();
+    if (this.production) {
+      htmlSource = htmlMinify(htmlSource, {
+        preserveLineBreaks: false,
+        removeComments: true,
+        removeCommentsFromCDATA: true,
+        collapseWhitespace: true,
+      });
+    }
+    this.createFile(compilation, file, htmlSource);
+    callback();
+};
+
+/* If the elementh with the tag includes a '!' prefix, it should be emitted */
+HtmlParserWebpackPlugin.prototype.parse = function(element, attr, compilation) {
+  var file = $(element).attr(attr);
+  // If the element does not have a valid href or src, or if it does not
+  // require bundling, don't do anything.
+  if (!file || file[0] !== '!') {
+    return;
+  }
+  $(element).attr(attr, this.emit(file.substring(1), compilation));
+};
+
+/* Create a file on the dist directory for the resource with a md5 name */
+HtmlParserWebpackPlugin.prototype.emit = function(url, compilation) {
+  var ext = url.split('.')[1];
+  var source = fs.readFileSync(path.join(this.compiler.context, url));
+  var filename = md5(source) + '.' + ext;
+  this.createFile(compilation, filename, source);
+  return filename;
+};
+
+HtmlParserWebpackPlugin.prototype.createFile = function(compilation, name, source) {
+  compilation.assets[name] = {
+    source: function() {
+      return source;
+    },
+    size: function() {
+      return source.length;
+    },
+  };
+}
+
+module.exports = HtmlParserWebpackPlugin;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+var webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    HtmlPlugin = require('./plugins/html-plugin'),
+    path = require('path');
+
+var getEnvironment = function() {
+  try {
+    // Try to import the environment module
+    return require('./environment.json');
+  } catch (error) {
+    // If it does not exist, return an empty object
+    if (error.code == 'MODULE_NOT_FOUND') {
+      return {};
+    }
+  }
+  // Return undefined if any other error occur
+  return undefined;
+};
+
+module.exports = {
+  devtool: 'source-map',
+
+  entry: {
+    'script': './scripts/index.js',
+    'style': './styles/index.less',
+  },
+
+  module: {
+    loaders: [
+      { test: /\.json$/, loader: 'json-loader'},
+      { test: /\.js$/, exclude: /(node_modules|bower_components)\//, loader: 'babel-loader'},
+      { test: /\.(ttf.*|eot.*|woff.*|ogg|mp3)$/, loader: 'file-loader'},
+      { test: /.(png|jpe?g|gif|svg.*)$/, loader: 'file-loader!img-loader?optimizationLevel=7&progressive=true'},
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader'),
+      },
+    ],
+  },
+
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new HtmlPlugin('index.html'),
+    new webpack.DefinePlugin({
+      Environment: JSON.stringify(getEnvironment()),
+    }),
+  ],
+
+  resolve: {
+    root: path.join(__dirname, 'js'),
+    extensions: ['', '.js', '.json'],
+  },
+
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+};
