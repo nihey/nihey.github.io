19d57651711880b2ee472149e726efab48ee8801
Nihey Takizawa <nihey.takizawa@gmail.com>
2015-10-01 20:31:53 -0300

    Update website animation

---
+++ style.js
@@ -0,0 +1,1 @@
+!function(e){function t(r){if(n[r])return n[r].exports;var i=n[r]={exports:{},id:r,loaded:!1};return e[r].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}([function(e,t){}]);
