307a86a1244b3a2ed7cd31fa03e59839ce4c4af1
Nihey Takizawa <nihey.takizawa@gmail.com>
2016-06-24 18:59:04 -0300

    Fix texts misalignment

--- style.js
+++ style.js
@@ -1,1 +1,1 @@
-!function(e){function t(r){if(n[r])return n[r].exports;var i=n[r]={exports:{},id:r,loaded:!1};return e[r].call(i.exports,i,i.exports,t),i.loaded=!0,i.exports}var n={};return t.m=e,t.c=n,t.p="",t(0)}([function(e,t){}]);
+!function(r){function t(o){if(e[o])return e[o].exports;var n=e[o]={exports:{},id:o,loaded:!1};return r[o].call(n.exports,n,n.exports,t),n.loaded=!0,n.exports}var e={};return t.m=r,t.c=e,t.p="",t(0)}([function(r,t){}]);
